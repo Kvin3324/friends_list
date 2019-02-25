deb8365fe4b945cc92e58ac62dc6fb7cd31f497f
kevin joya <kevin.joya@outlook.com>
2019-02-25 15:51:09 +0100

    Create index js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,10 @@
  * Import
  */
 import "./assets/scss/main.scss";
-import React from "react";
-import { render } from "react-dom";
 import App from "./view/app";
 
 /**
- * Variables
+ * APP
  */
-const app = document.querySelector("#root");
 
-/**
- * Déclaration
- */
-class Main extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <App/>
-            </React.Fragment>
-        );
-    }
-}
-
-/**
- * Éxécution
- */
-render(<Main/>, app);
+new App(); // TODO: New instance of App's class
